32f4bfb18fe24fd9e2a5c172dfb57a986c82d924
Myunghee <myunghee@usc.edu>
2020-11-05 19:34:25 -0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # DSCI560_HW5
+California Coronavirus Dashboard
+
+## Instructions for executing the dashboard on your computer
+1. Install Python 3.7.8
+2. Open your terminal and go to the directory you want to download this repository
+3. Download this repository in the directory
+```
+git clone https://github.com/Myunghee13/DSCI560_HW5.git
+```
+4. Go to the repository folder you downloaded 
+```
+cd DSCI560_HW5
+```
+5. Install virtualenv
+```
+pip install virtualenv
+```
+6. Create a virtual environment, named visu
+```
+py -m venv visu
+```
+7. Activate the virtual environment, visu
+```
+.\visu\Scripts\activate
+```
+8. Install the dependencies
+```
+pip install -r requirements.txt
+```
+9. Execute app.py
+```
+bokeh serve --show app.py
+```
+9. You can see the dashboard on your browser.
+```
+http://localhost:5006/app
+```
+10. Deactivate the virtual environment, visu
+```
+deactivate
+```
+## Instructions for executing the dashboard through Docker
+1. Go to [play with docker](https://labs.play-with-docker.com/).
+2. Log in and start.
+3. Add new instance
+
--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, show, output_notebook,ColumnDataSource,curdoc
-from bokeh.models import HoverTool,Select
+from bokeh.models import HoverTool, Select, Div
 from bokeh.layouts import row, column
 from bokeh.transform import dodge
-
-# from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, VBar
 
 data1 = pd.read_csv('latimes-state-totals.csv')
 
@@ -27,7 +25,7 @@ def make_plot1():
 
     source = ColumnDataSource(data=data)
 
-    p = figure(x_range=dates, plot_height=350, title="New Coronavirus cases in California",
+    p = figure(x_range=dates, plot_height=350, title="New Coronavirus cases in August in California",
                 toolbar_location=None, y_axis_label = 'New Confirmed Cases',
                  x_axis_label = 'August, 2020')
 
@@ -65,14 +63,14 @@ def get_dataset (date):
 
     data = {'races' : races,
             'confirmed' : list(df2['confirmed_cases_percent']),
-            'death'   : list(df2['deaths_percent']),
-            'population'   : list(df2['population_percent'])
+            'death' : list(df2['deaths_percent']),
+            'population' : list(df2['population_percent'])
            }
 
     return ColumnDataSource(data=data)
 
 def make_plot2(source):
-    p = figure(x_range=races, y_range=(0, 1), plot_height=250, title="Coronavirus cases % per race in California",
+    p = figure(x_range=races, y_range=(0, 1), plot_height=250, title="Coronavirus cases and deaths % per race in California",
                toolbar_location=None) #, tools="hover", tooltips="$name: @$name")
 
     p.vbar(x=dodge('races', -0.25, range=p.x_range), top='confirmed', width=0.2, source=source,
@@ -107,9 +105,42 @@ def update_plot(attrname, old, new):
 source = get_dataset (sel_date)
 plot2 = make_plot2(source)
 
-date_select = Select(value=sel_date, title='Date', options=date_list)
+date_select = Select(value=sel_date, title='Select Date', options=date_list)
 date_select.on_change('value', update_plot)
 
-curdoc().add_root(column(plot1, row(plot2, date_select)))
-curdoc().title = "Dashboard"
-# bokeh serve --show test.py
+
+div1 = Div(text="""
+<p><strong>Name: </strong>Myunghee Lee</p>
+<h1>1. Source for the data</h1>
+<p><strong>Source Link: </strong><a target="_blank" href="https://github.com/datadesk/california-coronavirus-data">The Los Angeles Times' independent tally of coronavirus cases in California.</a></p>
+<p><strong>Used files from the data source</strong></p>
+    <ol>
+        <strong><li>latimes-state-totals.csv</li></strong>
+        <p>The statewide total of cases and deaths logged by local public health agencies each day</p>
+        <p><strong>new_confirmed_cases: </strong>the net change in confirmed cases over the previous date.</p>
+        
+        <strong><li>cdph-race-ethnicity.csv: </li></strong>
+        <p>Statewide demographic data tallying race totals by age for both cases and deaths.</p>
+        <p>Provided by the <a target="_blank" href="https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/COVID-19/Race-Ethnicity.aspx">California Department of Public Health.</a></p>
+        <p><strong>race: </strong>The race being tallied</pi>
+        <p><strong>age: </strong>The age bracket being tallied, 0-17, 18+, 18-34, 35-49, 50-64, 65-79, 80+, <strong>all</strong>, I selected "all" for the graph.</p>       
+        <p><strong>confirmed_cases_percent: </strong>The case totals percentage of the total in this age bracket</p>
+        <p><strong>deaths_percent: </strong>The death totals percentage of the total in this age bracket.</p>
+        <p><strong>population_percent: </strong>The race's percentage of the overall state population in this age bracket.</p>
+    </ol>
+<h1>2. Date of last update</h1>
+<p>I downloaded the data from the source on <strong>November 5, 2020</strong>.</p>
+<h1>3. New coronavirus cases in August in California</h1>
+<p>You can see the number when you mouse over the bar.</p>
+""")
+#, width=1500, height=500)
+div2 = Div(text="""
+<h1>4. Cases and deaths % by race to their population %</h1>
+<p>You can see the number when you mouse over the bars.</p>
+<p>You can select the date by the "Select Date" button.</p>
+<p>No record if there is no date in the button.</p>
+""")
+
+curdoc().add_root(column(div1, plot1, div2, row(plot2, date_select)))
+curdoc().title = "California Coronavirus Dashboard"
+# bokeh serve --show app.py
